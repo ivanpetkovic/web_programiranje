e9ae7348b8f555af36e9bd61f21be86efa5a99d0
Ivan Petkovic <ivanpetkovic@Mac.local>
2024-11-25 10:50:24 +0100

    Initial commit

---
+++ 2_fje/src/closures2.js
@@ -0,0 +1,15 @@
+function outer() {
+  let i = 0;
+  const funcArray = [];
+  for ( i = 0; i < 5; i++) {
+    funcArray[i] = (function(curr) {
+      // console.log('i= ' + curr);
+      return () => {console.log('i=' + curr)};
+    })(i);
+  }
+  return funcArray;
+}
+const fa = outer();
+fa[0]();
+fa[1]();
+fa[2]();
